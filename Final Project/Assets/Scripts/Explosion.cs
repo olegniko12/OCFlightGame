fa36f4cd84af08578f75dc59655a18b05b11c076
olegniko12 <olegniko12@gmail.com>
2016-05-18 12:25:56 -0700

    Game

---
+++ Final Project/Assets/Scripts/Explosion.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	public GameObject explosion;
+
+	void OnCollisionEnter(){
+		GameObject expl = Instantiate (explosion, transform.position, Quaternion.identity) as GameObject;
+		Destroy (gameObject);
+		Destroy (expl, 10);
+	}
+}
---
+++ Final Project/Assets/Scripts/SplitScreen.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplitScreen : MonoBehaviour {
+
+	public bool Player1;
+	public bool Player2;
+
+}
